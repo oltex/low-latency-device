0180db96e60aa50fb3a2168075d582a5501d637a
User <User@DESKTOP-MRNC7IQ>
2023-08-14 23:57:30 +0900

    230814

--- tablet-service/Vmulti.cpp
+++ tablet-service/Vmulti.cpp
@@ -1,7 +1,7 @@
 #include "Vmulti.h"
 
 Vmulti::Vmulti(void)
-	: _device({ 0x00FF, 0xBACC, 0xFF00, 0x0001, FILE_WRITE_DATA })
+	: _device({ 0x00EE, 0xBACC, 0xFF00, 0x0001, FILE_WRITE_DATA })
 	/*_device()*/ {
 	memset(_buf, 0, 7);
 }
