67825073948d95ee7916a84f99a3186ea98fcddd
oltex <80347929+oltex@users.noreply.github.com>
2025-01-21 11:51:17 +0900

    250121

--- tablet-client/client.h
+++ tablet-client/client.h
@@ -1,7 +1,8 @@
 #pragma once
 #include "tablet.h"
-#include "vmulti.h"
+#include "mouse.h"
 #include "setting.h"
+#include <iostream>
 class client final {
 public:
 	inline explicit client(setting const& setting) noexcept
@@ -14,17 +15,56 @@ public:
 			if (!_tablet.read())
 				continue;
 
+			register unsigned char button = _tablet._buffer[1] & 0x7;
 			register unsigned short x = *reinterpret_cast<unsigned short*>(_tablet._buffer + 2);
 			register unsigned short y = *reinterpret_cast<unsigned short*>(_tablet._buffer + 4);
-			_vmulti._buffer[3] = _tablet._buffer[1] & 0x7;
-			*reinterpret_cast<unsigned short*>(_vmulti._buffer + 4) = (min(x, _setting._area._right) - _setting._area._left) * 32767 / _setting._area._width;
-			*reinterpret_cast<unsigned short*>(_vmulti._buffer + 6) = (min(y, _setting._area._bottom) - _setting._area._top) * 32767 / _setting._area._height;
 
-			_vmulti.write();
+			_mouse._input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+			if (!(1 & _mouse._button) && 1 & button)
+				_mouse._input.mi.dwFlags |= MOUSEEVENTF_LEFTDOWN;
+			if (1 & _mouse._button && !(1 & button))
+				_mouse._input.mi.dwFlags |= MOUSEEVENTF_LEFTUP;
+			_mouse._button = button;
+			_mouse._input.mi.dx = (min(x, _setting._area._right) - _setting._area._left) * 65535 / _setting._area._width;
+			_mouse._input.mi.dy = (min(y, _setting._area._bottom) - _setting._area._top) * 65535 / _setting._area._height;
+
+			_mouse.write();
 		}
 	};
+
 private:
 	tablet _tablet;
 	setting const _setting;
-	vmulti _vmulti;
+	mouse _mouse;
 };
+
+//#pragma once
+//#include "tablet.h"
+//#include "vmulti.h"
+//#include "setting.h"
+//class client final {
+//public:
+//	inline explicit client(setting const& setting) noexcept
+//		: _setting(setting) {
+//	};
+//	inline ~client(void) noexcept = default;
+//public:
+//	inline void run(void) noexcept {
+//		for (;;) {
+//			if (!_tablet.read())
+//				continue;
+//
+//			register unsigned short x = *reinterpret_cast<unsigned short*>(_tablet._buffer + 2);
+//			register unsigned short y = *reinterpret_cast<unsigned short*>(_tablet._buffer + 4);
+//			_vmulti._buffer[3] = _tablet._buffer[1] & 0x7;
+//			*reinterpret_cast<unsigned short*>(_vmulti._buffer + 4) = (min(x, _setting._area._right) - _setting._area._left) * 32767 / _setting._area._width;
+//			*reinterpret_cast<unsigned short*>(_vmulti._buffer + 6) = (min(y, _setting._area._bottom) - _setting._area._top) * 32767 / _setting._area._height;
+//
+//			_vmulti.write();
+//		}
+//	};
+//private:
+//	tablet _tablet;
+//	setting const _setting;
+//	vmulti _vmulti;
+//};
--- tablet-client/mouse.h
+++ tablet-client/mouse.h
@@ -7,12 +7,12 @@ class mouse final {
 public:
 	inline explicit mouse(void) noexcept {
 		_input.type = INPUT_MOUSE;
-		//_input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+		_input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
 	}
 	inline ~mouse(void) noexcept = default;
 public:
-	inline void write(void) const noexcept {
-		SendInput(1, (LPINPUT)&_input, sizeof(INPUT));
+	inline void write(void) noexcept {
+		SendInput(1, &_input, sizeof(INPUT));
 	}
 public:
 	INPUT _input{};
