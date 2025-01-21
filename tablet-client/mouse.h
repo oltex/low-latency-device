e29052a92a71a2115148a8ac5e425411b4139f1d
oltex <80347929+oltex@users.noreply.github.com>
2025-01-21 11:19:34 +0900

    250121

---
+++ tablet-client/mouse.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <WinUser.h>
+#include <SetupAPI.h>
+#pragma comment(lib, "user32.lib")
+
+class mouse final {
+public:
+	inline explicit mouse(void) noexcept {
+		_input.type = INPUT_MOUSE;
+		//_input.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+	}
+	inline ~mouse(void) noexcept = default;
+public:
+	inline void write(void) const noexcept {
+		SendInput(1, (LPINPUT)&_input, sizeof(INPUT));
+	}
+public:
+	INPUT _input{};
+	unsigned char _button = 0;
+};
