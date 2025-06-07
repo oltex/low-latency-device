477c10c7c2266d63d268c4e175cb598a851ab17d
oltex <80347929+oltex@users.noreply.github.com>
2025-06-07 11:55:45 +0900

    250607

--- audio-enhance/audio.h
+++ audio-enhance/audio.h
@@ -1,2 +1,43 @@
 #pragma once
 #include <Windows.h>
+#include <mmdeviceapi.h>
+#include <Audioclient.h>
+
+class audio final {
+public:
+	inline explicit audio(void) noexcept {
+		CoInitialize(nullptr);
+
+		IMMDeviceEnumerator* enumerator;
+		CoCreateInstance(__uuidof(MMDeviceEnumerator), nullptr, CLSCTX_ALL, __uuidof(IMMDeviceEnumerator), reinterpret_cast<void**>(&enumerator));
+
+		IMMDevice* device;
+		enumerator->GetDefaultAudioEndpoint(eRender, eConsole, &device);
+		enumerator->Release();
+
+		IAudioClient3* client;
+		device->Activate(__uuidof(IAudioClient3), CLSCTX_ALL, NULL, reinterpret_cast<void**>(&client));
+		device->Release();
+
+
+		//BOOL isOffloadCapable = FALSE;
+		//client->IsOffloadCapable(AudioCategory_Media, &isOffloadCapable);
+		//AudioClientProperties audioProps{};
+		//audioProps.cbSize = sizeof(AudioClientProperties);
+		//audioProps.eCategory = AudioCategory_Media;
+		//audioProps.Options = AUDCLNT_STREAMOPTIONS_RAW | AUDCLNT_STREAMOPTIONS_MATCH_FORMAT;
+		//client->SetClientProperties(&audioProps);
+
+		WAVEFORMATEX* format;
+		client->GetMixFormat(&format);
+
+		UINT32 default_period, fundamental_period, min_period, max_period;
+		client->GetSharedModeEnginePeriod(format, &default_period, &fundamental_period, &min_period, &max_period);
+
+		client->InitializeSharedAudioStream(0, min_period, format, nullptr);
+
+		CoTaskMemFree(format);
+		client->Release();
+		CoUninitialize();
+	}
+};
--- audio-enhance/main.cpp
+++ audio-enhance/main.cpp
@@ -1,5 +1,6 @@
-
+#include "audio.h"
 
 int main(int const argc, char const* const* const argv) noexcept {
+	audio audio;
 	return 0;
 }
