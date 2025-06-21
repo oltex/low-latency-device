e1086f9ffd0c7e8d7a2093625006dda10f2126e6
oltex <80347929+oltex@users.noreply.github.com>
2025-06-22 03:28:36 +0900

    250622

--- tablet-driver/mouse.h
+++ tablet-driver/mouse.h
@@ -16,3 +16,8 @@ public:
 
 	INPUT _input{};
 };
+
+//using NtSendInput = BOOLEAN(WINAPI*)(UINT cInputs, LPINPUT pInputs, int cbSize);
+//_nt_send_input = reinterpret_cast<NtSendInput>(GetProcAddress(GetModuleHandleW(L"win32u.dll"), "NtUserSendInput"));
+//_nt_send_input(1, &_input, sizeof(INPUT));
+//NtSendInput _nt_send_input;
