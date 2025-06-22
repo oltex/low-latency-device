9d711a124cbbb5bb3bfc19bb5ecb0e86bd26fc41
oltex <80347929+oltex@users.noreply.github.com>
2025-06-22 15:34:23 +0900

    250622

--- tablet-driver/tablet.h
+++ tablet-driver/tablet.h
@@ -4,13 +4,13 @@
 #include <hidsdi.h>
 #pragma comment(lib, "setupapi.lib")
 #include <SetupAPI.h>
-//#include <winternl.h>
-//
-//using NtReadFile = NTSTATUS(WINAPI*)(
-//	HANDLE FileHandle, HANDLE Event,
-//	PIO_APC_ROUTINE ApcRoutine, PVOID ApcContext,
-//	PIO_STATUS_BLOCK IoStatusBlock,
-//	PVOID Buffer, ULONG Length, PLARGE_INTEGER ByteOffset, PULONG Key);
+#include <winternl.h>
+
+using NtReadFile = NTSTATUS(WINAPI*)(
+	HANDLE FileHandle, HANDLE Event,
+	PIO_APC_ROUTINE ApcRoutine, PVOID ApcContext,
+	PIO_STATUS_BLOCK IoStatusBlock,
+	PVOID Buffer, ULONG Length, PLARGE_INTEGER ByteOffset, PULONG Key);
 
 class tablet final {
 public:
@@ -67,19 +67,19 @@ public:
 		HidD_SetFeature(_handle, buffer, 2);
 		HidD_SetNumInputBuffers(_handle, 2);
 
-		//_nt_read_file = reinterpret_cast<NtReadFile>(GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtReadFile"));
+		_nt_read_file = reinterpret_cast<NtReadFile>(GetProcAddress(GetModuleHandleW(L"ntdll.dll"), "NtReadFile"));
 	}
 
 	inline void const read(void) noexcept {
-		//IO_STATUS_BLOCK block;
+		IO_STATUS_BLOCK block;
 		do
-			ReadFile(_handle, _buffer, _report_length, nullptr, nullptr);
-			//_nt_read_file(_handle, nullptr, nullptr, nullptr, &block, _buffer, 10, &offset, nullptr);
+			//ReadFile(_handle, _buffer, _report_length, nullptr, nullptr);
+			_nt_read_file(_handle, nullptr, nullptr, nullptr, &block, _buffer, 10, &offset, nullptr);
 		while (_buffer[0] != _report_id || !(_buffer[1] & _detect_mask));
 	};
 
-	//NtReadFile _nt_read_file;
-	//LARGE_INTEGER offset{ 0 };
+	NtReadFile _nt_read_file;
+	LARGE_INTEGER offset{ 0 };
 	HANDLE /*__restrict*/ _handle;
 	alignas(2) unsigned char _buffer[10]{};
 	inline static constexpr unsigned char  _report_id = 0x02;
